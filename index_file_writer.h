5a81a5898206e6f623f425f626187217854eb966
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-11-30 14:57:49 -0500

    cleaning

--- index_file_writer.h
+++
@@ -1,4 +0,0 @@
-#include "hash.h"
-
-int write_formatted_hash_to_file(hash_table * table, char * file);
-
--- indexer.h
+++
@@ -1,5 +0,0 @@
-#include "hash.h"
-
-/* returns the name of the file that contains all the words */
-char * index_file(char * file, const char * root);
-struct hash_table * hash_table_from_word_count(char * file);
